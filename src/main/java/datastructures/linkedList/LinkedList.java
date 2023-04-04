e6ad75ed0442cdd6b938f8a9d726b39b4ab59305
Gabriel Jayme <gabrielj_89@hotmail.com>
2023-04-04 09:28:02 -0300

    Add insert method of LinkedList

--- src/main/java/datastructures/linkedList/LinkedList.java
+++ src/main/java/datastructures/linkedList/LinkedList.java
@@ -114,15 +114,6 @@ public class LinkedList {
     return true;
   }
 
-  public boolean set(int index, int value) {
-    Node temp = get(index);
-    if (temp != null) {
-      temp.value = value;
-      return false;
-    }
-    return false;
-  }
-
   public Node get(int index) {
     if (index < 0 || index >= length) {
       return null;
